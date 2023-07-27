eab8d87f68c8cb7a4298ed2802e67ea7180c34a6
Nico <odelotsalocin@gmail.com>
2023-07-27 12:07:45 -0300

    edit form created in the edit component

--- master en react/06-project01/src/components/Editar.js
+++ master en react/06-project01/src/components/Editar.js
@@ -1,7 +1,26 @@
 import React from 'react'
 
 export const Editar = () => {
+    const titulo_componenete = "Editar Pelicula";
+
+
   return (
-    <div>Editar</div>
+    <div className="edit_form">
+        <h3 className="title">{titulo_componenete}</h3>
+        <form>
+            <input type="text"
+                    name="titulo"
+                    className="titulo_editado"
+                    defaultValue="Titulo original de la pelicula"
+            />
+            <textarea
+                name="descripcion"
+                defaulValue="Descripcion original"
+                className="descripcion_editada"
+            />
+            <input type='submit' className='editar' value="Actualizar" />
+     
+        </form>
+    </div>
   )
 }
