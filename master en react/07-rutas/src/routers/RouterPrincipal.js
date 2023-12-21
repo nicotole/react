7a6540306e4b2a73b2dc419ca099b619e915647a
Nico <odelotsalocin@gmail.com>
2023-12-21 11:54:18 -0300

    Implemente una barra de navegacion para hacer una pagina SPA usando React Router

--- master en react/07-rutas/src/routers/RouterPrincipal.js
+++ master en react/07-rutas/src/routers/RouterPrincipal.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Routes, Route, Link, BrowserRouter} from 'react-router-dom';
+import { Routes, Route, NavLink, BrowserRouter} from 'react-router-dom';
 import { Inicio } from '../components/Inicio'
 import { Contacto } from '../components/Contacto'
 import { Articulos } from '../components/Articulos'
@@ -8,13 +8,35 @@ import { Error } from '../components/Error';
 export const RouterPrincipal = () => {
   return (
     <BrowserRouter>
-        <Routes>
+        <h1>Cabecera</h1>
+        <hr />
+
+        <nav>
+          <ul>
+            <li>
+              <NavLink to="/inicio">Inicio</NavLink> {/* Navega a Inicio*/}
+            </li>
+            <li>
+              <NavLink to="/contacto">Contacto</NavLink>
+            </li>
+            <li>
+              <NavLink to="/articulos">Articulo</NavLink>
+            </li>
+          </ul>
+        </nav>
+        
+        <hr />
+
+        <Routes>{/* El componente Routes, renderiza un elemento segun la ruta escrita en el navegador */}
             <Route path="/" element={<Inicio />} />
             <Route path="inicio" element={<Inicio />} />
             <Route path="contacto" element={<Contacto />} />
             <Route path="articulos" element={<Articulos />} />
             <Route path="*" element={<Error />} />  
         </Routes>
+
+        <hr />
+        <h2>Footer</h2>
     </BrowserRouter>
   )
 }
