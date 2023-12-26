e53cbf7c2992ceaa1f919deaed5109094a24002f
Nico <odelotsalocin@gmail.com>
2023-12-26 12:11:28 -0300

    Hook useNavigate

--- master en react/07-rutas/src/components/Persona.js
+++ master en react/07-rutas/src/components/Persona.js
@@ -1,14 +1,36 @@
 import React from 'react'
-import { useParams } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
 export const Persona = () => {
 
     const { nombre, apellido } = useParams();
+    const navegar = useNavigate()
 
+    const enviar = (e) =>{
+        e.preventDefault();
+        let nombre = e.target.nombre.value;
+        let apellido = e.target.apellido.value; 
+        let url = `/persona/${nombre}/${apellido}`
+        
+        if(nombre.length <= 0 && apellido.length <= 0){
+            navegar("/inicio");
+
+        } else if (nombre === "contacto"){
+            navegar("/contacto");
+        } else {
+            navegar(url);
+        }
+    };
+    
   return (
     <div>
         {!nombre && <h1>No hay ninguna persona que mostrar</h1>}
         {nombre && <h1>Pagina de persona: {nombre} {apellido} </h1>}
 
+        <form onSubmit={enviar}>
+            <input type="text" name="nombre" />
+            <input type="text" name="apellido" />
+            <input type="submit" name="enviar" value="enviar" /> 
+        </form>
     </div>
   )
 }
