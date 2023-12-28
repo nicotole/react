2e6ff496c304972bf411436379b486f0449d0bfe
Nico <odelotsalocin@gmail.com>
2023-12-28 12:24:30 -0300

    Barra de navegación para navegar entre rutas con Navlink

--- master en react/08-proyecto2/src/components/layout/HeaderNav.js
+++ master en react/08-proyecto2/src/components/layout/HeaderNav.js
@@ -1,16 +1,43 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 export const HeaderNav = () => {
   return (
     
     <header>
-        <h1>Nicolás Toledo DEV</h1>
+        <div className="logo">
+            <span>
+                DEV
+            </span>
+            <h3>Nicolás Toledo</h3>
+        </div>
         <nav>
             <ul>
                 <li>
-                    
+                    <NavLink to="/inicio">Inicio</NavLink>
                 </li>
             </ul>
+            <ul>
+                <li>
+                    <NavLink to="/portafolio">Portafolio</NavLink>
+                </li>
+            </ul>
+            <ul>
+                <li>
+                    <NavLink to="/servicios">Servicios</NavLink>
+                </li>
+            </ul>
+            <ul>
+                <li>
+                    <NavLink to="/curriculum">Curriculum</NavLink>
+                </li>
+            </ul>
+            <ul>
+                <li>
+                    <NavLink to="/contacto">Contacto</NavLink>
+                </li>
+            </ul>
+
         </nav>
     </header>
   )
