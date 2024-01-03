aacf583dfbf1993c486e5260c1689744063d534d
Nico <odelotsalocin@gmail.com>
2024-01-03 10:58:07 -0300

    creando array de objetos de proyectos

---
+++ master en react/08-proyecto2/src/Data/trabajos.js
@@ -0,0 +1,23 @@
+export const trabajos = [
+    {
+        'id' : 'linkeado',
+        'nombre' : 'Linkeado',
+        'url' : 'linkenado.netlify.app',
+        'tecnologias' : 'HTML, CSS, JS, REACT',
+        'categorias' : 'Desarrollo Web'
+    },
+    {
+        'id' : 'facebook',
+        'nombre' : 'Facebook',
+        'url' : 'facebook.com',
+        'tecnologias' : 'REACT, NODE, MONGO',
+        'categorias' : 'Desarrollo Web'
+    },
+    {
+        'id' : 'twitter',
+        'nombre' : 'Twitter',
+        'url' : 'twitter.com',
+        'tecnologias' : 'HTML, CSS',
+        'categorias' : 'Maquetacion web'
+    }
+]
--- master en react/08-proyecto2/src/components/Portafolio.js
+++ master en react/08-proyecto2/src/components/Portafolio.js
@@ -1,7 +1,19 @@
 import React from 'react'
+import { trabajos } from '../Data/trabajos' 
 
 export const Portafolio = () => {
   return (
-    <div>Portafolio</div>
+    <div className='page'>
+      <h1 className='heading'></h1>
+    {
+      trabajos.map(trabajo=> {
+        <article>
+          <h2>
+            {trabajo.nombre}
+          </h2>
+        </article>
+      })
+    }
+    </div>
   )
 }
