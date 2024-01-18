3c0780a51fcea0b52ef3e72cdd1d9e0a025c23be
Nico <odelotsalocin@gmail.com>
2024-01-18 11:26:25 -0300

    lista de tareas con carga dinamica

--- master en react/10-memorizacion/src/components/Tareas.js
+++ master en react/10-memorizacion/src/components/Tareas.js
@@ -1,13 +1,30 @@
-import React from 'react'
+import React, {  useState } from 'react'
 
 export const Tareas = () => {
+
+  const [tareas , setTareas] = useState([]);
+
+  const guardarTareas = (e) =>{
+    e.preventDefault();
+    setTareas( tarea => [...tarea, e.target.descripcion.value ])
+  }
+
   return (
     <div className='tareas-container'>
         <h1> Mis tareas </h1>
-        <form>
-            <input type='text' placeholder='Descripcion' />
+        <form onSubmit={guardarTareas}>
+            <input type='text' name='descripcion' placeholder='Descripcion' />
             <input type='submit' value='Guardar'/>
         </form>
+
+        <h3>Lista de tareas</h3>
+        <ul>
+          {
+            tareas.map((tarea, indice) => {
+              return <li key={indice}>{tarea}</li>
+            })
+          }
+        </ul>
     </div>
   )
 }
