887c42bed603a8416c95b66dd39851a55bde2809
Nico <odelotsalocin@gmail.com>
2024-01-19 10:09:42 -0300

    Eliminar tareas - useMemo

--- master en react/10-memorizacion/src/components/Tareas.js
+++ master en react/10-memorizacion/src/components/Tareas.js
@@ -9,6 +9,13 @@ export const Tareas = () => {
     setTareas( tarea => [...tarea, e.target.descripcion.value ])
   }
 
+  const borrarTarea = (id) => {
+    //filtrar tareas para borrar la que no quiero
+    let nuevas_tareas = tareas.filter( (tarea, indice) => indice !== id); //filtrar = quitar
+    //guardar el nuevo listado en el estado
+    setTareas(nuevas_tareas);
+  } 
+
   return (
     <div className='tareas-container'>
         <h1> Mis tareas </h1>
@@ -21,7 +28,12 @@ export const Tareas = () => {
         <ul>
           {
             tareas.map((tarea, indice) => {
-              return <li key={indice}>{tarea}</li>
+              return (
+                <li key={indice}>
+                  {tarea}
+                  <button onClick={() => borrarTarea(indice)}>x</button>
+                </li>
+              )
             })
           }
         </ul>
